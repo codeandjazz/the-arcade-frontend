89a33aa4da99da072ddebb49c73dc03cbc30efa2
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-02 20:26:24 +0200

    minimum boilerplate

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <section className="hero">HERO</section>
+        <section className="featured">FEATURED</section>
+        <section className="new">NEW GAMES</section>
+        <section className="popular">POPULAR</section>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default LandingPage;
