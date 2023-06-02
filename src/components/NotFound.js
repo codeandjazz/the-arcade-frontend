5d2f3abf31f0c68bdaa9eb15c6e514f4afd63b22
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-02 20:08:22 +0200

    added minimum boilerplate of the components

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    // Add image as background and style h1 with glitch effect
+    <div className="404">
+      <h1>Game over</h1>
+    </div>
+  );
+};
+
+export default NotFound;
