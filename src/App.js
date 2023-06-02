540a1b45979083240e17410e05005cff1bace8ef
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-02 19:46:49 +0200

    start of routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 export const App = () => {
-  return <div>Find me in src/app.js!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>} />
+        {/* ↑↑↑ This is the home page ↑↑↑ */}
+        <Route path="/details/:id" element={<h1>Details</h1>} />
+        {/* ↑↑↑ This is a fake route ↑↑↑ */}
+        <Route path="/404" element={<h1>Not found</h1>} />
+        {/* ↑↑↑ Insert 404component here ↑↑↑ */}
+        <Route path="*" element={<Navigate to="/404" replace />} />
+        {/* ↑↑↑ This routs to 404 if no route is found ↑↑↑ */}
+      </Routes>
+    </BrowserRouter>
+  );
 };
