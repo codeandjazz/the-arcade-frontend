8d42c3db42e837a82e3692194e6fd1fbb9de210d
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-04 16:25:27 +0200

    updated the landingpage with radomGames component

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import GamesDisplay10 from './GamesDisplay10';
 
 const LandingPage = () => {
   return (
@@ -8,6 +9,7 @@ const LandingPage = () => {
       <Header />
       <main>
         <section className="hero">HERO</section>
+        <GamesDisplay10 />
         <section className="featured">FEATURED</section>
         <section className="new">NEW GAMES</section>
         <section className="popular">POPULAR</section>
