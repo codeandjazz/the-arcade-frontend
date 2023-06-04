5df326af3dcd076207610fe47dc909302eaaa778
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-04 17:10:06 +0200

    smaller fixes of GameDisplay10

--- src/components/GamesDisplay10.js
+++ src/components/GamesDisplay10.js
@@ -2,25 +2,31 @@
 /* eslint-disable operator-linebreak */
 /* eslint-disable no-underscore-dangle */
 import React, { useState, useEffect } from 'react';
-import { Card, Grid, Row, Text, Link } from '@nextui-org/react';
+import { Card, Grid, Row, Text, Link, Loading } from '@nextui-org/react';
 import defaultImg from '../assets/img/default-img.png';
 
 const GamesDisplay10 = () => {
   const [storedGames, setStoredGames] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  // Fetch the games from the API when the component mounts
   useEffect(() => {
     const fetchGames = async () => {
       try {
+        // Get the games from the API
         const response = await fetch('http://localhost:8080/games');
         const data = await response.json();
         const getRandomGames = (games, count) => {
+          // Shuffle the games
           const shuffled = games.sort(() => 0.5 - Math.random());
           return shuffled.slice(0, count);
         };
         if (data.success) {
           const games = data.response;
+          // Get 10 random games
           const randomGames = getRandomGames(games, 10);
+
+          // Store the games in state
           setStoredGames(randomGames);
         } else {
           console.log(data.message);
@@ -39,11 +45,11 @@ const GamesDisplay10 = () => {
       <h1>10 random games</h1>
       <Grid.Container gap={1} justify="flex-start" direction="row">
         {loading ? (
-          <p>Loading...</p>
+          <Loading type="points" />
         ) : (
           storedGames.map((game, index) => (
             <Grid key={game._id}>
-              <Link href={`/games/${game.id}`}>
+              <Link href={`/games/${game.slug}/${game._id}`}>
                 <Card isPressable>
                   <Card.Body css={{ p: 0 }}>
                     {game.cover && game.cover.url ? (
