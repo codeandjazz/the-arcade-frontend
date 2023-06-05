6abac74a124c626a607be8c33c81abcd2f11c70b
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-05 12:04:28 +0200

    added navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import user from 'reducers/user';
+import { Navbar, Button, Link, Text, Image } from '@nextui-org/react';
+import Logo from '../assets/img/logo-the-arcade.png';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <section>
+      <Navbar isCompact isBordered variant="sticky">
+        <Navbar.Brand>
+          <Image src={Logo} width={100} height={50} alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Content hideIn="xs" variant="underline">
+          <Navbar.Link href="#">Game</Navbar.Link>
+          <Navbar.Link isActive href="#">
+            Games
+          </Navbar.Link>
+          <Navbar.Link href="#">Game</Navbar.Link>
+          <Navbar.Link href="#">Game</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Link color="inherit" href="#">
+            Login
+          </Navbar.Link>
+          <Navbar.Item>
+            <Button auto flat as={Link} href="#">
+              Sign Up
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
+      </Navbar>
+    </section>
+  );
 };
 
 export default Header;
