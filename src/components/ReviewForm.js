7cfce347915503b28ac3bbfd0a2f2c608266e322
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-06 15:43:49 +0200

    try of debugging

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -3,44 +3,50 @@
 import React, { useState } from 'react';
 import { API_URL } from 'utils/urls';
 
-const ReviewForm = ({ gameId }) => {
-  const [review, setReview] = useState('');
-  //   const [rating, setRating] = useState(0);
+const ReviewForm = ({ id }) => {
+  const [message, setMessage] = useState('');
   const [userId, setUserId] = useState(null);
+  //   const [rating, setRating] = useState(0);
+  console.log('this is the id: ', id);
 
   const onFormSubmit = async (event) => {
     event.preventDefault();
     console.log('clicked!');
 
     try {
-      const response = await fetch(API_URL(`/games/${gameId}/reviews`), {
+      const response = await fetch(API_URL(`games/${id}/reviews`), {
         method: 'POST',
         headers: {
           'content-type': 'application/json'
         },
         body: JSON.stringify({
-          review,
+          message,
           userId
         })
       });
+
+      if (!response.ok) {
+        throw new Error('Request failed with status ', response.status);
+      }
       const data = await response.json();
       console.log(data); // this is the response from the server
     } catch (error) {
-      console.error(error);
+      console.error('this is the error: ', error);
     }
-
-    return (
-      <form>
-        <label htmlFor="review">Review</label>
-        <input
-          type="text"
-          id="review"
-          value={review}
-          onChange={(e) => setReview(e.target.value)}
-        />
-      </form>
-    );
   };
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <label htmlFor="review">message</label>
+      <input
+        type="text"
+        id="review"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default ReviewForm;
