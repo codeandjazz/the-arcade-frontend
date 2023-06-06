f21640c71c999c4b94846c07f90b61f545ece87b
tess.aquilon <tess.aquilon@gmail.com>
2023-06-06 12:39:30 +0200

    added more routes

--- src/App.js
+++ src/App.js
@@ -31,6 +31,12 @@ export const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/games/:slug/:id" element={<Game />} />
           {/* ↑↑↑ This is a game page ↑↑↑ */}
+          <Route path="/games" element={<GamesList />} />
+          {/* ↑↑↑ This is the games list page ↑↑↑ */}
+          <Route path="/users/:id" element={<UserProfile />} />
+          {/* ↑↑↑ This is a user profile page ↑↑↑ */}
+          <Route path="/games/:id" element={<SingleGame />} />
+          {/* ↑↑↑ This is a single game page ↑↑↑ */}
           <Route path="/404" element={<NotFound />} />
           {/* ↑↑↑ Insert 404component here ↑↑↑ */}
           <Route path="*" element={<Navigate to="/404" replace />} />
