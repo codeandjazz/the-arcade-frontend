63b4cc01ca3c57e3d9dd8330e55eb47776722a41
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-07 22:37:20 +0200

    somewhat working review form

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,35 +1,40 @@
+/* eslint-disable quote-props */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-closing-bracket-location */
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { API_URL } from 'utils/urls';
 
 const ReviewForm = ({ id }) => {
   const [message, setMessage] = useState('');
-  const [userId, setUserId] = useState(null);
   //   const [rating, setRating] = useState(0);
-  console.log('this is the id: ', id);
 
+  const accessToken = useSelector((store) => store.user.accessToken);
+  const userId = useSelector((store) => store.user.user_id);
   const onFormSubmit = async (event) => {
     event.preventDefault();
-    console.log('clicked!');
+    // console.log('clicked!');
 
     try {
-      const response = await fetch(API_URL(`games/${id}/reviews`), {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json'
-        },
-        body: JSON.stringify({
-          message,
-          userId
-        })
-      });
+      const response = await fetch(
+        API_URL('games/647b64ecadae08c30fe91f4a/reviews'),
+        {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': accessToken
+          },
+          body: JSON.stringify({
+            message,
+            userId
+          })
+        }
+      );
 
       if (!response.ok) {
         throw new Error('Request failed with status ', response.status);
       }
       const data = await response.json();
-      console.log(data); // this is the response from the server
     } catch (error) {
       console.error('this is the error: ', error);
     }
