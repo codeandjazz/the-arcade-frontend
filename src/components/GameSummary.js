9f2297f71ba38310395d6f5adeed311b9d250e74
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-07 22:45:45 +0200

    hide/show of review

--- src/components/GameSummary.js
+++ src/components/GameSummary.js
@@ -1,11 +1,17 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable react/jsx-closing-bracket-location */
-import React from 'react';
-import { Container, Image } from '@nextui-org/react';
+import React, { useState } from 'react';
+import { Container, Image, Button } from '@nextui-org/react';
 import ReviewForm from './ReviewForm';
 
 const GameSummary = ({ game }) => {
+  const [showReviewForm, setShowReviewForm] = useState(false);
+  console.log(showReviewForm);
   console.log(game);
+
+  const handleShowReviewForm = () => {
+    setShowReviewForm(!showReviewForm);
+  };
   return (
     <section>
       <Container width="100%" margin="0 auto">
@@ -16,7 +22,8 @@ const GameSummary = ({ game }) => {
           <h1>sadsa</h1>
           <p>[Summary here]</p>
           {/* Add summary, note not all games has summary */}
-          <ReviewForm id={game._id} />
+          <Button onPress={handleShowReviewForm}>Write a review</Button>
+          {showReviewForm && <ReviewForm />}
         </div>
       </Container>
     </section>
