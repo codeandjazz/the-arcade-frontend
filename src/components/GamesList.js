00b4f92e2da5ee514d174536498095523c680c06
tess.aquilon <tess.aquilon@gmail.com>
2023-06-07 16:54:07 +0200

    more work on gamesList

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-underscore-dangle */
 import React, { useEffect, useState } from 'react';
-import { Grid, Loading, Link, Card, Row, Text, Container } from '@nextui-org/react';
+import { Grid, Loading, Link, Card, Row, Text, Container, Pagination, Dropdown } from '@nextui-org/react';
 import { API_URL } from 'utils/urls';
 import defaultImg from '../assets/img/default-img.png';
 import Header from './Header';
 
 const GamesList = () => {
   const [storedGames, setStoredGames] = useState([]);
+  const [storedGenres, setStoredGenres] = useState([]);
   const [loading, setLoading] = useState(true);
 
   // Fetch the games from the API when the component mounts
@@ -28,14 +29,50 @@ const GamesList = () => {
         console.error(error);
       }
     };
-
+    const fetchGenres = async () => {
+      try {
+        // Get the genres from the API
+        const response = await fetch(API_URL('games/genres'));
+        const data = await response.json();
+        if (data.success) {
+          const genres = data.response;
+          // Store the genres in state
+          setStoredGenres(genres);
+        } else {
+          console.log(data.message);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
     fetchGames();
+    fetchGenres();
   }, []);
+
+  // Dropdown menu items
+  const menuItems = [
+    { key: 'new', name: 'New File' },
+    { key: 'copy', name: 'Copy Link' },
+    { key: 'edit', name: 'Edit File' },
+    { key: 'delete', name: 'Delete File' }
+  ];
 
   return (
     <>
       <Header />
       <Container md>
+        <Dropdown>
+          <Dropdown.Button>Categories</Dropdown.Button>
+          <Dropdown.Menu aria-label="Dynamic Actions" items={menuItems}>
+            {(item) => (
+              <Dropdown.Item
+                key={item.key}>
+                {item.name}
+              </Dropdown.Item>
+            )}
+          </Dropdown.Menu>
+        </Dropdown>
         <Grid.Container gap={1} justify="center" direction="row">
           {loading ? (
             <Loading type="points" />
@@ -73,6 +110,7 @@ const GamesList = () => {
             ))
           )}
         </Grid.Container>
+        <Pagination total={20} initialPage={1} />
       </Container>
     </>
   );
