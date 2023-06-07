908dd65e8b6953fc2f101ece0b989d184f7829c9
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-07 22:36:59 +0200

    added props to Review form

--- src/components/GameSummary.js
+++ src/components/GameSummary.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react/jsx-closing-bracket-location */
 import React from 'react';
 import { Container, Image } from '@nextui-org/react';
+import ReviewForm from './ReviewForm';
 
-const GameSummary = () => {
+const GameSummary = ({ game }) => {
+  console.log(game);
   return (
     <section>
       <Container width="100%" margin="0 auto">
@@ -13,6 +16,7 @@ const GameSummary = () => {
           <h1>sadsa</h1>
           <p>[Summary here]</p>
           {/* Add summary, note not all games has summary */}
+          <ReviewForm id={game._id} />
         </div>
       </Container>
     </section>
