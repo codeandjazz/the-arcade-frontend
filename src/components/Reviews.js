9a5ccefd5359a00e246710f04433173043b27085
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-08 23:15:59 +0200

    fix of accesstoken in Reviews.js

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-underscore-dangle */
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 const Reviews = () => {
   const [review, setReview] = useState([]);
+
+  // get accestoken from store
+  const accessToken = useSelector((store) => store.user.accessToken);
 
   const fetchReviews = async () => {
     try {
@@ -31,8 +35,7 @@ const Reviews = () => {
           method: 'DELETE',
           headers: {
             'Content-Type': 'application/json',
-            Authorization:
-              '618d80cdb6c1ca5d4aadda4d0f599ad6cfc506d672f18f85dcedb3f7a0bf108695240b29dfdd2654a0d517a569785ee0b19fb239cd307587308b6d85c6c2101e89ae0f0c7ee661ef7fb476853a12f8d03e344f35859332a7170c11500075c5499515b4506417b221287fed8472d501bbd0e1584ab7ce6e033eda8e52a21975ea'
+            Authorization: accessToken
           }
         }
       );
