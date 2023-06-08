a6b246d379aa4289bfde24b1152d6b76f75ecd3f
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-08 21:44:22 +0200

    fix of url

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable quote-props */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-closing-bracket-location */
@@ -6,10 +7,10 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { API_URL } from 'utils/urls';
 
-const ReviewForm = ({ showReviewForm, setShowReviewForm }) => {
+const ReviewForm = ({ showReviewForm, setShowReviewForm, game }) => {
   const [message, setMessage] = useState('');
   //   const [rating, setRating] = useState(0);
-
+  console.log(game);
   const accessToken = useSelector((store) => store.user.accessToken);
   // console.log(accessToken);
   const userId = useSelector((store) => store.user.user_id);
@@ -20,25 +21,23 @@ const ReviewForm = ({ showReviewForm, setShowReviewForm }) => {
     // console.log('clicked!');
 
     try {
-      const response = await fetch(
-        API_URL('games/647b64ecadae08c30fe91f4a/reviews'),
-        {
-          method: 'POST',
-          headers: {
-            'content-type': 'application/json',
-            'Authorization': accessToken
-          },
-          body: JSON.stringify({
-            message,
-            userId
-          })
-        }
-      );
+      const response = await fetch(API_URL(`games/${game._id}/reviews`), {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': accessToken
+        },
+        body: JSON.stringify({
+          message,
+          userId
+        })
+      });
 
       if (!response.ok) {
         throw new Error('Request failed with status ', response.status);
+      } else {
+        const data = await response.json();
       }
-      const data = await response.json();
     } catch (error) {
       console.error('this is the error: ', error);
     }
