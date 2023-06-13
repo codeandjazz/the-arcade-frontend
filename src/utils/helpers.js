e167c1ce4536731a8f6893983af9ea06534a983e
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-13 22:56:41 +0200

    formatDate to helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,10 @@
 /* Add helper functions here, its nice */
+export const formatDate = (string) => {
+  const dateString = string;
+  const date = new Date(dateString);
+
+  const options = { day: 'numeric', month: 'long', year: 'numeric' };
+  const formattedDate = date.toLocaleDateString('en-US', options);
+
+  return formattedDate;
+};
