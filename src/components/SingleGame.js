9f2cff2804d80d14273cbbb65420900d79569187
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-16 08:54:07 +0200

    the fix

--- src/components/SingleGame.js
+++ src/components/SingleGame.js
@@ -1,137 +1,97 @@
-/* eslint-disable no-underscore-dangle */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable prefer-template */
+/* eslint-disable object-shorthand */
+/* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable react/jsx-closing-bracket-location */
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import {
-  Container,
-  Image,
-  Button,
-  Col,
-  Row,
-  Spacer,
-  Text
-} from '@nextui-org/react';
-import { API_URL } from 'utils/urls';
-import ReviewForm from './ReviewForm';
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Container, Image, css, Loading } from '@nextui-org/react';
+import axios, { isCancel, AxiosError } from 'axios';
+import NotFoundImg from '../assets/img/not-found-404.jpg';
+import GameSummary from './GameSummary';
+import { API_URL } from '../utils/urls';
 
-const GameSummary = ({ game }) => {
-  const [showReviewForm, setShowReviewForm] = useState(false);
-  // console.log(showReviewForm);
-  // console.log(game.cover.url);
+import Header from './Header';
 
-  const timestamp = game.first_release_date;
-  const date = new Date(timestamp * 1000);
-  const releaseDate = date.toLocaleDateString();
-  const handleShowReviewForm = () => {
-    setShowReviewForm(!showReviewForm);
-  };
-  const accessToken = useSelector((store) => store.user.accessToken);
-  // Patch request to add a favorite game to the user
-  const HandleAddFavorite = () => {
-    const options = {
-      method: 'PATCH',
-      headers: {
-        'content-type': 'application/json',
-        Authorization: accessToken
+const Game = () => {
+  const { slug, id } = useParams();
+  const [game, setGame] = useState({});
+  const [prompt, setPrompt] = useState('');
+  const [imageURL, setImage] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const createImg = async (imageDescription) => {
+    const response = await axios.post(
+      'https://the-arcade-backend-6426jh4m2a-no.a.run.app/create',
+      {
+        prompt: imageDescription
       }
-    };
-    fetch(API_URL(`games/${game._id}/addfavorite`), options)
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.success) {
-          console.log(data);
-        }
-      });
+    );
+    setImage(response.data);
+    console.log(imageURL);
+    setLoading(false);
   };
 
+  const handleChange = (e) => {
+    setPrompt(e.target.value);
+  };
+
+  const fetchGameBasedOnId = async () => {
+    try {
+      const response = await fetch(
+        `https://the-arcade-backend-6426jh4m2a-no.a.run.app/games/${id}`
+      )
+        // const response = await fetch(`${API_URL(`games/${id}`)}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data.response);
+          if (data.success) {
+            setGame(data.response);
+            console.log(game);
+            setPrompt(data.response.name);
+            setLoading(false);
+          } else {
+            console.log(data.message);
+          }
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGameBasedOnId();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (game.name) {
+      createImg(game.name + 'retro video game');
+    }
+  }, [game]);
   return (
-    <section>
-      <Container
-        md
-        width="100%"
-        gap={0}
-        css={{ marginTop: '-100px', zIndex: '$max' }}
-      >
-        <Row gap={1}>
-          {game?.cover?.url && ( // Check if cover URL is available
-            <Col>
-              <Image
-                src={game.cover.url.replace('t_thumb', 't_cover_big')}
-                alt={`Cover art of the game ${game.name}`}
-                width={220}
-                height={380}
-                objectFit="cover"
-              />
-              <Container>
-                <Spacer y={0.5} />
-                <Button
-                  size="lg"
-                  onPress={handleShowReviewForm}
-                  css={{ margin: '0 auto' }}
-                >
-                  Write a review
-                </Button>
-                <Spacer y={1} />
-                <Button
-                  size="lg"
-                  disabled={!accessToken}
-                  onPress={HandleAddFavorite}
-                  css={{ margin: '0 auto' }}
-                >
-                  ❤️ Add to favorites
-                </Button>
-                <Spacer y={0.5} />
-                <Button
-                  size="lg"
-                  disabled={!accessToken}
-                  onPress={HandleAddFavorite}
-                  css={{ margin: '0 auto' }}
-                >
-                  Remove from favorites
-                </Button>
-              </Container>
-            </Col>
-          )}
-          <Col>
-            <Container css={{ marginTop: '100px' }}>
-              <h1>{game.name}</h1>
-              {/* Map out game genres if available */}
-              {game.genres &&
-                game.genres.map((genre) => (
-                  <Button
-                    key={genre.id}
-                    css={{
-                      backgroundColor: '$purple400',
-                      fontSize: '$sm',
-                      fontWeight: '$bold',
-                      margin: '$2',
-                      padding: '$1'
-                    }}
-                  >
-                    <Link to={`/games/genres/${genre.name}`}>
-                      {genre.name} &nbsp;
-                    </Link>
-                  </Button>
-                ))}
-              {/* Show release date and summary if they are not undefined */}?{' '}
-              {releaseDate} : <p>Release date: {releaseDate}</p> :{' '}
-              <p>Release date: Unknown</p>? {game.summary} :{' '}
-              <p>{game.summary}</p> : <p>No summary available</p>
-              {/* Add summary, note not all games has summary */}
-              {showReviewForm && (
-                <ReviewForm
-                  setShowReviewForm={setShowReviewForm}
-                  showReviewForm={showReviewForm}
-                  game={game}
-                />
-              )}
-            </Container>
-          </Col>
-        </Row>
+    <>
+      <Header />
+      <Container width="100%" margin="0 auto">
+        {loading && <Loading type="points" />}
+        {imageURL && (
+          <Image // This could be a AI generated image based on the game name
+            showSkeleton
+            src={imageURL}
+            alt={game.name}
+            css={{
+              maxHeight: '300px',
+              width: '100%',
+              objectFit: 'cover'
+            }}
+          />
+        )}
+        {game && <GameSummary game={game} />}
       </Container>
-    </section>
+    </>
   );
 };
 
-export default GameSummary;
+export default Game;
