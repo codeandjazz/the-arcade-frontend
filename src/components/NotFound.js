3f44874fd11c151c1c9cf3e3f8638335e5f09c87
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-20 19:57:47 +0200

    style of 404

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,37 @@
+/* eslint-disable react/jsx-closing-bracket-location */
 import React from 'react';
+import { Container, Text, Grid, css, textTransforms } from '@nextui-org/react';
+import { Link } from 'react-router-dom';
+
+import NotFound404 from '../assets/img/not-found-404.jpg';
 
 const NotFound = () => {
   return (
     // Add image as background and style h1 with glitch effect
-    <div className="404">
-      <h1>Game over</h1>
-    </div>
+    <Container
+      className="404"
+      css={{
+        background: ` no-repeat center url(${NotFound404})`,
+        height: '100vh'
+      }}
+    >
+      <Grid.Container
+        justify="center"
+        alignItems="center"
+        direction="column"
+        css={{ height: '100vh' }}
+      >
+        <Text h1 css={{ color: 'White', textDecoration: 'underline' }}>
+          Game over
+        </Text>
+        <Text h2 css={{ color: 'White' }}>
+          404 - Page not found
+        </Text>
+        <Link to="/" css={{ color: 'White' }}>
+          Go home
+        </Link>
+      </Grid.Container>
+    </Container>
   );
 };
 
