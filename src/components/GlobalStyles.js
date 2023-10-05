5a16066eb794f2394f661df5422f69c43a42875d
augustaquilon <august.aquilon@gmail.com>
2023-10-05 16:51:48 +0200

    renovating

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -1,6 +1,9 @@
 import styled, { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+* {
+    box-sizing: border-box;
+  }
 body {
     background-color: #000;
     color: #fff;
